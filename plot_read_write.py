118cb5373976bb75a44514495acc0cb13b90b8dc
Laurent Montigny <lmontigny@users.noreply.github.com>
2018-04-11 16:37:29 +0200

    Update plot_read_write.py

--- plot_read_write.py
+++ plot_read_write.py
@@ -1,3 +1,18 @@
+#!/usr/bin/env python
+"""
+======================
+Read and write graphs.
+======================
+Read and write graphs.
+"""
+# Author: Aric Hagberg (hagberg@lanl.gov)
+
+#    Copyright (C) 2004-2018 by
+#    Aric Hagberg <hagberg@lanl.gov>
+#    Dan Schult <dschult@colgate.edu>
+#    Pieter Swart <swart@lanl.gov>
+#    All rights reserved.
+#    BSD license.
 
 import sys
 
